12c4d05fc210b21c6f9e8b6756bcf05640a7d99c
Daio-io <dai.williams@bbc.co.uk>
2015-10-31 21:25:21 +0000

    Updated AlarmModel to be a RealmObject and created an AlarmRepository

--- app/src/main/java/io/daio/earnthatsnooze/models/AlarmModel.java
+++ app/src/main/java/io/daio/earnthatsnooze/models/AlarmModel.java
@@ -1,30 +1,50 @@
 package io.daio.earnthatsnooze.models;
 
-import android.net.Uri;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
-public class AlarmModel {
+public class AlarmModel extends RealmObject {
 
     public static int MONDAY = 1, TUESDAY = 2, WEDNESDAY = 3,
         THURSDAY = 4, FRIDAY = 5, SATURDAY = 6, SUNDAY = 7;
 
-    public long id;
-    public int hour;
-    public int minute;
-    public Uri alertTone;
-    public boolean isEnabled;
+    @PrimaryKey
+    private long id;
 
-    private boolean[] daysToRepeat;
+    private int hour;
+    private int minute;
+    private boolean isEnabled = false;
 
-    public AlarmModel() {
-        daysToRepeat = new boolean[7];
+    public long getId() {
+        return id;
     }
 
-    public void setDayToRepeat(int weekDay, boolean value) {
-        daysToRepeat[weekDay] = value;
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public boolean getDayToRepeat(int weekDay) {
-        return daysToRepeat[weekDay];
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
     }
 
 }
---
+++ app/src/main/java/io/daio/earnthatsnooze/repository/AlarmRepository.java
@@ -0,0 +1,57 @@
+package io.daio.earnthatsnooze.repository;
+
+import android.support.annotation.Nullable;
+
+import io.daio.earnthatsnooze.App;
+import io.daio.earnthatsnooze.models.AlarmModel;
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public final class AlarmRepository {
+
+    private static AlarmRepository alarmRepo;
+    private Realm realm = Realm.getInstance(App.getAppContext());
+
+    private AlarmRepository() {}
+
+    public static AlarmRepository getInstance() {
+        if (alarmRepo == null) {
+            alarmRepo = new AlarmRepository();
+        }
+        return alarmRepo;
+    }
+
+    public void save(AlarmModel model) {
+        realm.beginTransaction();
+        realm.copyToRealmOrUpdate(model);
+        realm.commitTransaction();
+    }
+
+    @Nullable
+    public Iterable<AlarmModel> getAll() {
+        return realm.where(AlarmModel.class)
+                .findAll();
+    }
+
+    @Nullable
+    public AlarmModel getById(long id) {
+        RealmResults<AlarmModel> models = realm.where(AlarmModel.class)
+                .equalTo("id", id)
+                .findAll();
+
+        if (models.size() == 0) {
+            return null;
+        }
+        return models.first();
+    }
+
+    public void removeById(long id) {
+        realm.beginTransaction();
+        RealmResults<AlarmModel> results = realm.where(AlarmModel.class)
+                .equalTo("id", id)
+                .findAll();
+
+        results.clear();
+        realm.commitTransaction();
+    }
+}
