5170cbcde11bc844a92f6b8f4e3f2bd52a5795de
Daio-io <dai.williams@bbc.co.uk>
2015-10-31 19:09:48 +0000

    Expose App Context on App class

--- app/src/main/java/io/daio/earnthatsnooze/App.java
+++ app/src/main/java/io/daio/earnthatsnooze/App.java
@@ -1,12 +1,21 @@
 package io.daio.earnthatsnooze;
 
 import android.app.Application;
+import android.content.Context;
 
 public class App extends Application {
+
+    private static Context mContext;
 
     @Override
     public void onCreate(){
         super.onCreate();
+
+        mContext = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return mContext;
     }
 
 }
