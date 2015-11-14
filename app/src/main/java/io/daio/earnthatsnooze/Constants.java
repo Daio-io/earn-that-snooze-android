29783459025834446a20f6d286d46ba9b5f69759
Daio-io <dai.williams@bbc.co.uk>
2015-11-14 16:59:02 +0000

    Removed alarm name from model

--- app/src/main/java/io/daio/earnthatsnooze/Constants.java
+++ app/src/main/java/io/daio/earnthatsnooze/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 
     // ** ALARM MANAGER CONSTANTS ** //
     public static final String ALARM_ID_KEY = "ALARM_ID";
-    public static final String ALARM_NAME_KEY = "ALARM_NAME";
     public static final String ALARM_HOUR_KEY = "ALARM_HOUR";
     public static final String ALARM_MINUTE_KEY = "ALARM_MINUTE";
     public static final String ALARM_ENABLED_KEY = "ALARM_ENABLED";
--- app/src/main/java/io/daio/earnthatsnooze/models/AlarmModel.java
+++ app/src/main/java/io/daio/earnthatsnooze/models/AlarmModel.java
@@ -7,29 +7,20 @@ import io.realm.annotations.PrimaryKey;
 public class AlarmModel extends RealmObject {
 
     @PrimaryKey
-    private int id;
+    private long id;
 
-    private String alarmName;
     private int hour;
     private int minute;
     private RealmList<WeekDayModel> repeatingDays;
 
     private boolean isEnabled = false;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
-    }
-
-    public String getAlarmName() {
-        return alarmName;
-    }
-
-    public void setAlarmName(String name) {
-        this.alarmName = name;
     }
 
     public int getHour() {
