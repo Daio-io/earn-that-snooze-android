469968af29cc666dae8ea2c75e5bb59e1505df5a
Daio-io <dai.williams@bbc.co.uk>
2015-11-22 21:59:11 +0000

    Rename and clean up CalendarUtil

--- app/src/main/java/io/daio/earnthatsnooze/utils/CalendarUtil.java
+++ app/src/main/java/io/daio/earnthatsnooze/utils/CalendarUtil.java
@@ -2,7 +2,9 @@ package io.daio.earnthatsnooze.utils;
 
 import java.util.Calendar;
 
-public class CalenderUtil {
+public class CalendarUtil {
+
+    private static Calendar calendar = Calendar.getInstance();
 
     public static boolean isToday(int dateToCheck) {
 
@@ -25,19 +27,21 @@ public class CalenderUtil {
         int dayOfWeek = getCurrentDay();
 
         if (dayOfWeek == Calendar.SATURDAY) {
-            return dayOfWeek < dateToCheck;
+            return dayOfWeek <= dateToCheck;
         } else {
-            return dayOfWeek > dateToCheck;
+            return dayOfWeek >= dateToCheck;
         }
 
     }
 
-    public static boolean hasTimePassed(int hour, int minute) {
-        Calendar calendar = Calendar.getInstance();
+    public static boolean hasTimePassed(int hour, int minute, int second) {
         int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
         int currentMinute = calendar.get(Calendar.MINUTE);
+        int currentSecond = calendar.get(Calendar.MILLISECOND);
 
-        return currentHour > hour || currentHour <= hour && currentMinute > minute;
+        return currentHour > hour
+                || currentHour == hour && currentMinute > minute
+                || currentHour == hour && currentMinute == minute && currentSecond > second;
     }
 
     private static int getCurrentDay() {
